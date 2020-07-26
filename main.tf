69bef2d90bc281f516a1680077bda4deb9877ca0
thrucvoid19 <thrucovid19@gmail.com>
2020-07-26 12:58:49 -0500

    Removed iam user

--- main.tf
+++ main.tf
@@ -13,36 +13,6 @@ provider "aws" {
   region = var.region
 }
 
-provider "template" {
-}
-
-resource "aws_iam_user" "circleci" {
-  name = var.user
-  path = "/system/"
-}
-
-resource "aws_iam_access_key" "circleci" {
-  user = aws_iam_user.circleci.name
-}
-
-data "template_file" "circleci_policy" {
-  template = file("circleci_s3_access.tpl.json")
-  vars = {
-    s3_bucket_arn = aws_s3_bucket.portfolio.arn
-  }
-}
-
-resource "local_file" "circle_credentials" {
-  filename = "tmp/circleci_credentials"
-  content  = "${aws_iam_access_key.circleci.id}\n${aws_iam_access_key.circleci.secret}"
-}
-
-resource "aws_iam_user_policy" "circleci" {
-  name   = "AllowCircleCI"
-  user   = aws_iam_user.circleci.name
-  policy = data.template_file.circleci_policy.rendered
-}
-
 resource "aws_s3_bucket" "portfolio" {
   tags = {
     Name = "Portfolio Website Bucket"
